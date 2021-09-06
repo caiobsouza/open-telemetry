bf1f7071bcffe81a93dc62ac23315b9f102e7b22
Caio Souza <caiobsouza@hotmail.com>
2021-09-06 14:37:54 -0300

    build: initial typescript and linting configuration

---
+++ .eslintrc.js
@@ -0,0 +1,39 @@
+module.exports = {
+    'env': {
+        'es2021': true,
+        'node': true
+    },
+    'extends': [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended'
+    ],
+    'parser': '@typescript-eslint/parser',
+    'parserOptions': {
+        'ecmaVersion': 12,
+        'sourceType': 'module'
+    },
+    'plugins': [
+        '@typescript-eslint'
+    ],
+    'rules': {
+        'no-console': [
+            'error',
+            { 'allow': ['warn', 'error'] }],
+        'indent': [
+            'error',
+            4
+        ],
+        'linebreak-style': [
+            'error',
+            'unix'
+        ],
+        'quotes': [
+            'error',
+            'single'
+        ],
+        'semi': [
+            'error',
+            'always'
+        ]
+    }
+};
