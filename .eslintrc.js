03c93c1435b0bfe4dd27e374442dce3ff3f3a7c0
Caio Souza <caiobsouza@hotmail.com>
2021-09-06 16:42:33 -0300

    chore: define basic types and documentation tool

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,43 +1,43 @@
 module.exports = {
-    'env': {
-        'es2021': true,
-        'node': true
-    },
-    'extends': [
-        'eslint:recommended',
-        'plugin:@typescript-eslint/recommended'
+  'env': {
+    'es2021': true,
+    'node': true
+  },
+  'extends': [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended'
+  ],
+  'parser': '@typescript-eslint/parser',
+  'parserOptions': {
+    'ecmaVersion': 12,
+    'sourceType': 'module'
+  },
+  'plugins': [
+    '@typescript-eslint'
+  ],
+  'rules': {
+    'no-console': [
+      'error',
+      { 'allow': ['warn', 'error'] }],
+    'indent': [
+      'error',
+      2
     ],
-    'parser': '@typescript-eslint/parser',
-    'parserOptions': {
-        'ecmaVersion': 12,
-        'sourceType': 'module'
-    },
-    'plugins': [
-        '@typescript-eslint'
+    'linebreak-style': [
+      'error',
+      'unix'
     ],
-    'rules': {
-        'no-console': [
-            'error',
-            { 'allow': ['warn', 'error'] }],
-        'indent': [
-            'error',
-            4
-        ],
-        'linebreak-style': [
-            'error',
-            'unix'
-        ],
-        'quotes': [
-            'error',
-            'single'
-        ],
-        'semi': [
-            'error',
-            'always'
-        ],
-        'eol-last': [
-            'error',
-            'always'
-        ]
-    }
+    'quotes': [
+      'error',
+      'single'
+    ],
+    'semi': [
+      'error',
+      'always'
+    ],
+    'eol-last': [
+      'error',
+      'always'
+    ]
+  }
 };
---
+++ src/car-telemetry/CarTelemetry.types.ts
@@ -0,0 +1,96 @@
+/**
+ * @module CarTelemetry
+ */
+
+import { PacketHeader } from '../common/PacketHeader.types';
+
+export interface CarTelemetry {
+  /**
+   * Speed of car in kilometres per hour
+   */
+  m_speed: number
+
+  /**
+   * Amount of throttle applied (0.0 to 1.0)
+   */
+  m_throttle: number
+
+  /**
+   * Steering (-1.0 (full lock left) to 1.0 (full lock right))
+   */
+  m_steer: number
+
+  /**
+   * Amount of brake applied (0.0 to 1.0)
+   */
+  m_brake: number
+
+  /**
+   * Amount of clutch applied (0 to 100)
+   */
+  m_clutch: number
+
+  /**
+   * Gear selected (1-8, N=0, R=-1)
+   */
+  m_gear: number
+
+  /**
+   * Engine RPM
+   */
+  m_engineRPM: number
+
+  m_drs: DrsStatus
+
+  /**
+   * Rev lights indicator (percentage)
+   */
+  m_revLightsPercent: number
+
+  /**
+   * Brakes temperature (celsius) for each wheel (4)
+   */
+  m_brakesTemperature: Array<number>
+
+  /**
+   * Tyres surface temperature (celsius)
+   */
+  m_tyresSurfaceTemperature: Array<number>
+
+  /**
+   * Tyres inner temperature (celsius)
+   */
+  m_tyresInnerTemperature: Array<number>
+
+  /**
+   * Engine temperature (celsius)
+   */
+  m_engineTemperature: number
+
+  /**
+   * Tyres pressure (PSI)
+   */
+  m_tyresPressure: Array<number>
+
+  /**
+   * Driving surface, see appendices
+   */
+  m_surfaceType: Array<number>
+}
+
+export enum DrsStatus {
+  OFF = 0,
+  ON = 1
+}
+
+/**
+ * This packet details telemetry for all the cars in the race.
+ */
+export interface PacketCarTelemetryData {
+  m_header: PacketHeader
+  m_carTelemetryData: CarTelemetry
+  /**
+   * Suggested gear for the player (1-8). 0 if no gear suggested.
+   */
+  m_suggestedGear: number
+}
